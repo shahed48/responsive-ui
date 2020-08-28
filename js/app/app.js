6afadd2e31d9779792710709bb9ab18e75744539
shahed48 <shahjahan.cseku11@gmail.com>
2020-08-28 10:26:05 +0600

    initial commit

---
+++ js/app/app.js
@@ -0,0 +1,24 @@
+﻿
+// function UpdateSelect(e, selection) {
+//     $(".nav-anchor").each(function (index, e) {
+//         if ($(this).hasClass('active')) {
+//             $(this).removeClass('active').removeClass('arrow');
+//         }
+//     });
+//     $(e.target).addClass('active').addClass('arrow');
+
+//     var action_name = 'Dashboard';
+//     if (selection === 'profile') { action_name = 'UserProfile'; }
+//     else if (selection === 'works') { action_name = 'Works'; }
+//     else if (selection === 'dashboard') { action_name = 'Dashboard' }
+//     else {
+//         $('#SiteContent').empty().html('<div class="base-view"<h2>UNDER CONSTRUCTION</h2></div>');
+//         return;
+//     }
+// }
+
+// $("ul#asideNav li:first-child a").click();
+
+// function Logout() {
+//     document.getElementById('logoutForm').submit();
+// }
